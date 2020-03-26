795e8178cab24c11ca1284eee578e29f6ae5c89b
Unperterbed <33593261+Unperterbed@users.noreply.github.com>
2020-03-25 22:47:26 -0700

    Added subscales
    Added the "subscales" argument to calculate 0, 2, 6, or 8 factors for the CVQ.

--- R/getCVQ.R
+++ R/getCVQ.R
@@ -6,9 +6,10 @@
 #' @param condensed Optional Boolean (defaults TRUE). Indicates whether the scale and item should be combined (e.g., "CVQ18") or in seperate columns (e.g., "CVQ" and "18"). Note that this ONLY applies when longformat = TRUE.
 #' @param cutoff Optional Numerical, 0 - 1 (defaults 0.8) Indicates the percentage of required responses be a "qualified" case. For instance, if cutoff = 0.8 then they must have answered at >=80 percent of the possible items. Enter "0" to include all entries.
 #' @param reportmissing Optional Boolean (defaults TRUE). Indicates whether to include "nMissing" and "pMissing" columns to indicate the number and percentage of items missing, respectively.
-#' @param reverse Optional Boolean (defaults TRUE). Indicates whether to reverse score the CVQ-8 item or not.
 #' @param new_id Optional Boolean (defaults FALSE). Indicates whether to combine year and ID into a new ID formatted as Year_ID.
 #' @param drop_id Optional Boolean (defaults FALSE). Indicates whether to drop the original ID column.Note that this ONLY applies when new_id = TRUE.
+#' @param reverse Optional Boolean (defaults TRUE). Indicates whether to reverse score the CVQ-8 item or not.
+#' @param subscales Optional numeric (defaults '0'). Indicates the subscales to calculate. Valid options are 0, 2, 6, or 8. The 8-factor option simply returns both the 2- and 6-factor scales. The 0 option doesn't return any subscales. Be aware that item 8 MUST be reverse scored to calculate correctly; you may use "Reverse = TRUE" to do it here, or leave "Reverse = FALSE" if you did it before importing the data. See Dik et al (2012) for details on scoring.
 #' @keywords CVQ
 #' @export
 #' @examples getCVQ()
@@ -18,20 +19,24 @@
 # Need to specify version control for required packages
 # Sample dataset
 # Test functions
+# 'Factor' argument to compute factor scores (might need multiple string options depending on whether they want the 2- or 3-factor scale)
+# 'Scale' argument to compute a total scale score
 
 getCVQ <- function(RawDataSet,
                    longformat = FALSE,
                    condensed = TRUE,
                    cutoff = .8,
                    reportmissing = TRUE,
-                   reverse = TRUE,
                    new_id = FALSE,
-                   drop_id = FALSE){
+                   drop_id = FALSE,
+                   reverse = TRUE,
+                   subscales = 0){
 
   #Argument validation
   stopifnot(missing(RawDataSet)==FALSE)
   stopifnot(class(RawDataSet)=="data.frame" | class(RawDataSet)=="matrix")
   stopifnot(cutoff >= 0 & cutoff <=1)
+  stopifnot(subscales == 0 | subscales == 2 | subscales == 6 | subscales == 8)
 
 
   #Requires dplyr and tidyverse to be installed and activated. If either are missing, it installs tidyverse but activates tidyr and dplyr
@@ -104,6 +109,55 @@ getCVQ <- function(RawDataSet,
     tempDF <- tempDF %>%
       select(-c(nMissing, pMissing))
   }
+
+  #Calculates subscale scores, if requested
+  if(subscales != 0){
+    print("NOTE: The subscales assume that CVQ8 has been reverse scored. If needed, you can include the argument Reverse = TRUE.")
+
+    #Creates all 8 scales
+
+      #Adds the correct CVQ8 to CVQ_Pres_TranSummons
+      if(sum(colnames(tempDF)=="CVQ8_R")==1){
+        #uses CVQ8_R
+        tempDF <- tempDF %>%
+          mutate(CVQ_Pres_TranSummons = CVQ1 + CVQ8_R + CVQ11 + CVQ23)
+      }else{
+        #uses CVQ8
+        tempDF <- tempDF %>%
+          mutate(CVQ_Pres_TranSummons = CVQ1 + CVQ8 + CVQ11 + CVQ23)
+      }
+
+      #Adds the other 7 scales
+      tempDF <- tempDF %>%
+        mutate(CVQ_Pres_PurpWork = CVQ3 + CVQ15 + CVQ20 + CVQ24) %>%
+        mutate(CVQ_Pres_ProSoc = CVQ9 + CVQ12 + CVQ17 + CVQ22) %>%
+        mutate(CVQ_Search_TranSummons = CVQ2 + CVQ13 + CVQ18 + CVQ19) %>%
+        mutate(CVQ_Search_PurpWork = CVQ4 + CVQ6 + CVQ14 + CVQ21) %>%
+        mutate(CVQ_Search_ProSoc = CVQ5 + CVQ7 + CVQ10 + CVQ16) %>%
+        mutate(CVQ_Presence = CVQ_Pres_TranSummons + CVQ_Pres_PurpWork + CVQ_Pres_ProSoc) %>%
+        mutate(CVQ_Searching = CVQ_Search_TranSummons + CVQ_Search_PurpWork + CVQ_Search_ProSoc)
+
+
+
+    if(subscales == 2){
+      #Drops the six scales
+      tempDF <- tempDF %>%
+        select(-c(CVQ_Pres_TranSummons,
+                  CVQ_Pres_PurpWork,
+                  CVQ_Pres_ProSoc,
+                  CVQ_Search_TranSummons,
+                  CVQ_Search_PurpWork,
+                  CVQ_Search_ProSoc))
+      }
+
+    if(subscales == 6){
+      #Drops the two scales
+      tempDF <- tempDF %>%
+        select(-c(CVQ_Searching,
+                  CVQ_Presence))
+      }
+
+    }
 
   #Converts to long format, if requested
   if (longformat == TRUE){
